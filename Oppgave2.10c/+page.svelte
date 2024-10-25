14f93f95d6121009e8cba209d38e2432ad0850e5
overas <37910044+overas@users.noreply.github.com>
2024-10-25 12:29:58 +0200

    Create +page.svelte
    Kode uten knapp

---
+++ Oppgave2.10c/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { gjsnittTallene } from "./gjenbruk/mineFunksjoner.js";
+    let tall = "";
+</script>
+
+<label>
+    <h3>Program for å regne ut gjennomsnitt av flere tall:</h3>
+    Skriv tallene separert med komma:
+    <input bind:value={tall} />
+</label>
+
+<p>Gjennomsnitt av tallene er: {gjsnittTallene(tall)}</p>
