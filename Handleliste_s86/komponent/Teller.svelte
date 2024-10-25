84fb1f946f71483964092aa43213dca50f3a7bd8
overas <37910044+overas@users.noreply.github.com>
2024-10-25 14:59:18 +0200

    Create Teller.svelte

---
+++ Handleliste_s86/komponent/Teller.svelte
@@ -0,0 +1,13 @@
+<!-- komponent/Teller.svelte -->
+<script> 
+    export let navn; 
+    export let verdi = 0;
+
+</script>
+
+<span>
+    {navn}:
+    <button on:click={() => (verdi = verdi - 1)}>-</button>
+    {verdi}
+    <button on:click={() => (verdi = verdi + 1)}>+</button>
+</span>
