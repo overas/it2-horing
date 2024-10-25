01b28797d48e43b399ac7eaaf95a6d97cb8a9944
overas <37910044+overas@users.noreply.github.com>
2024-10-25 14:58:33 +0200

    Create +page.svelte

---
+++ Handleliste_s86/+page.svelte
@@ -0,0 +1,26 @@
+<!-- handleliste/+page.svelte -->
+<script>
+    import Teller from "./komponent/Teller.svelte";
+
+    let varer = [];
+    let vare = "";
+</script>
+
+<h1>Handleliste</h1>
+<label>Legg til ny vare: <input bind:value={vare} /></label>
+
+<ul>
+    {#each varer as vare}
+        <li><Teller bind:navn={vare.navn} bind:verdi={vare.antall} /></li>
+    {/each}
+
+    <li>
+        <button
+            on:click={() => {
+                varer = [...varer, { navn: vare, antall: 0 }];
+            }}
+        >
+            Legg til vare
+        </button>
+    </li>
+</ul>
