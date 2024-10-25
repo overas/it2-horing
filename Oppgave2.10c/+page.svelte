f0bba7c22eb350fa857bb4f29d12ab20f7842f2f
overas <37910044+overas@users.noreply.github.com>
2024-10-25 12:37:17 +0200

    Update +page.svelte
    Kode med knapp til å kjøre funksjon

--- Oppgave2.10c/+page.svelte
+++ Oppgave2.10c/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import { gjsnittTallene } from "./gjenbruk/mineFunksjoner.js";
+    let gjsnitt = 0;
     let tall = "";
 </script>
 
@@ -7,6 +8,7 @@
     <h3>Program for å regne ut gjennomsnitt av flere tall:</h3>
     Skriv tallene separert med komma:
     <input bind:value={tall} />
+    <button on:click={() => (gjsnitt = gjsnittTallene(tall)})>Regn ut gjennomsnitt</button>
 </label>
 
 <p>Gjennomsnitt av tallene er: {gjsnittTallene(tall)}</p>
